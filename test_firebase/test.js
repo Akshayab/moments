f58e4af11322efd29cdf83e748eed40b4782d1a7
Akshay Budhkar <akshaybudhkar@v1020-wn-238-98.campus-dynamic.uwaterloo.ca>
2014-09-20 00:47:22 -0400

    Set up some login and signup shit with firebase

--- test_firebase/test.js
+++ test_firebase/test.js
@@ -1,20 +1,23 @@
-var fb = new Firebase("https://torid-inferno-6582.firebaseio.com/moments");
-var fb_user = new Firebase("https://torid-inferno-6582.firebaseio.com/moments/users");
-
-fb.once('value', function(data){
-console.log(data.val());
-});	
-
-var user2 = {
-		"name": "Akshay",
-		"email_id": "abudhkar@uwaterloo.ca",
-		"moments": []
-};
-
-var user2path = new Firebase("https://torid-inferno-6582.firebaseio.com/moments/users/" + user2.name);
-user2path.set(user2);
-var user_path = fb_user.child("1").toString();
-var user_1 = new Firebase(user_path);
-user_1.once('value', function(data){
-console.log(data.val());
+var myRef = new Firebase("https://torid-inferno-6582.firebaseio.com/");
+      var authClient = new FirebaseSimpleLogin(myRef, function(error, user) {
+      if (error) {
+        // an error occurred while attempting login
+        console.log(error);
+      } else if (user) {
+        // user authenticated with Firebase
+        console.log("User ID: " + user.uid + ", Provider: " + user.provider);
+      } else {
+        // user is logged out
+      }
+    });
+      authClient.createUser('abudhkar2@uwaterloo.ca', 'newpassword123!', function(error, user) {
+  if (error === null) {
+    console.log("User created successfully:", user);
+  } else {
+    console.log("Error creating user:", error);
+  }
+});
+      authClient.login('password', {
+  email: 'abudhkar@uwaterloo.ca',
+  password: 'newpassword123!'
 });
